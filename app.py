eee5e27ed81be054da5ae155835a285693507c87
protmaks <9646520@gmail.com>
2026-03-07 22:30:48 +0100

    fix

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ menu = {
         st.Page("menu/monitoring/jobs_in_allpurp_cluster.py", title="Jobs in All-purp Cluster", icon=":material/memory:",),
     ],
     "Jobs": [
-        st.Page("menu/jobs_and_pipelines/jobs_settings.py", title="Job Settings", icon=":material/check_circle:",),
+        st.Page("menu/jobs_and_pipelines/jobs_settings.py", title="Check Job Settings", icon=":material/check_circle:",),
         st.Page("menu/jobs_and_pipelines/jobs_run_daily.py", title="Jobs Runs (Daily)", icon=":material/grid_view:"),
         st.Page("menu/jobs_and_pipelines/jobs_timeline_hourly.py", title="Jobs Timeline (Hourly)", icon=":material/schedule:"),
         st.Page("menu/jobs_and_pipelines/jobs_fails_details.py", title="Job Fails Details", icon=":material/bug_report:"),
